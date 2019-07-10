a86ae1e3a587a0b7338ea89d3416f5c8b9c66f77
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-07-10 20:40:45 +0300

    Make dynamic students list

--- students/views.py
+++ students/views.py
@@ -1,10 +1,30 @@
+# -*- coding: utf-8 -*-
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Views for students
 
 def students_list(request):
-    return render(request, 'students/students_list.html', {})
+    students = (
+        {'id': 1,
+         'first_name': u'Андрій',
+         'last_name': u'Вірт',
+         'ticket': 2501,
+         'image': 'img/student_1.jpg'},
+        {'id': 2,
+         'first_name': u'Юрій',
+         'last_name': u'Семчук',
+         'ticket': 2548,
+         'image': 'img/student_2.jpg'},
+        {'id': 3,
+         'first_name': u'Роман',
+         'last_name': u'Ярощак',
+         'ticket': 2557,
+         'image': 'img/student_3.jpg'},
+        )
+    return render(request, 'students/students_list.html',
+        {'students': students})
 
 def students_add(request):
     return HttpResponse('<h1>Student Add Form</h1>')
