b62a1c972952e272be6be6f3434ad540e07e571a
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-07-27 16:02:43 +0300

    Update view for groups

--- students/views/groups.py
+++ students/views/groups.py
@@ -2,16 +2,34 @@
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from ..models import Group
 
 def groups_list(request):
-    groups = (
-        {'id': 1,
-         'name': u'ІМЗ-21',
-         'leader': {'id': 1, 'name': u'Олег Скочиляс'}},
-        {'id': 2,
-         'name': u'ІМЗ-22',
-         'leader': {'id': 2, 'name': u'Софія Михайлів'}},
-    )
+    groups = Group.objects.all()
+
+    # try to order groups list
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('title', 'leader'):
+        groups = groups.order_by(order_by)
+        if request.GET.get('reverse', '') == '1':
+            groups = groups.reverse()
+    else:
+        order_by = 'title'
+        groups = groups.order_by(order_by)
+
+    # paginate groups
+    paginator = Paginator(groups, 3)
+    page = request.GET.get('page')
+    try:
+        groups = paginator.page(page)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver first page.
+        groups = paginator.page(1)
+    except EmptyPage:
+        # if page is out of range (e.g. 9999), deliver last page of results.
+        groups = paginator.page(paginator.num_pages)
     return render(request, 'students/groups_list.html',
          {'groups': groups})
 
