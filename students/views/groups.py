cab347746bc93b415a06e9ff8cc02867c2246927
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-08-26 10:30:16 +0300

    Add class-based view for edit groups and Crispy Forms styles to group edit form

--- students/views/groups.py
+++ students/views/groups.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.urlresolvers import reverse
+from django.forms import ModelForm
+from django.views.generic import UpdateView
+
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from crispy_forms.bootstrap import FormActions
 
 from ..models import Group
 
@@ -36,8 +43,49 @@ def groups_list(request):
 def groups_add(request):
     return HttpResponse('<h1>Group Add Form</h1>')
 
-def groups_edit(request, gid):
-    return HttpResponse('<h1>Edit Group %s</h1>' % gid)
+class GroupUpdateForm(ModelForm):
+    class Meta:
+        model = Group
+
+    def __init__(self, *args, **kwargs):
+        super(GroupUpdateForm, self).__init__(*args, **kwargs)
+
+        self.helper = FormHelper(self)
+
+        # set form tag attributes
+        self.helper.form_action = reverse('groups_edit',
+            kwargs={'pk': kwargs['instance'].id})
+        self.helper.form_method = 'POST'
+        self.helper.form_class = 'form-horizontal'
+
+        # set form field properties
+        self.helper.help_text_inline = True
+        self.helper.html5_required = True
+        self.helper.label_class = 'col-sm-2 control-label'
+        self.helper.field_class = 'col-sm-10'
+
+        # add buttons
+        self.helper.layout[-1] = FormActions(
+            Submit('add_button', u'Зберегти', css_class="btn btn-primary"),
+            Submit('cancel_button', u'Скасувати', css_class="btn btn-link"),
+        )
+
+class GroupUpdateView(UpdateView):
+    model = Group
+    template_name = 'students/groups_edit.html'
+    form_class = GroupUpdateForm
+
+    def get_success_url(self):
+        return u'%s?status_message=Групу успішно збережено!' \
+            % reverse('groups')
+
+    def post(self, request, *args, **kwargs):
+        if request.POST.get('cancel_button'):
+            return HttpResponseRedirect(
+                u'%s?status_message=Редагування групи відмінено!' %
+                reverse('groups'))
+        else:
+            return super(GroupUpdateView, self).post(request, *args, **kwargs)
 
 def groups_delete(request, gid):
     return HttpResponse('<h1>Delete Group %s</h1>' % gid)
