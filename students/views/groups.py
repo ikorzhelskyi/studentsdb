6812481d0ed78beec6ef88b535ebbce8a22272a5
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-08-31 13:14:52 +0300

    Reorganize class-based views for groups

--- students/views/groups.py
+++ students/views/groups.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.urlresolvers import reverse
 from django.forms import ModelForm, ValidationError
-from django.views.generic import UpdateView, DeleteView
+from django.views.generic import CreateView, UpdateView, DeleteView
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
@@ -40,33 +40,44 @@ def groups_list(request):
     return render(request, 'students/groups_list.html',
          {'groups': groups})
 
-def groups_add(request):
-    return HttpResponse('<h1>Group Add Form</h1>')
-
-class GroupUpdateForm(ModelForm):
+class GroupForm(ModelForm):
     class Meta:
         model = Group
 
     def __init__(self, *args, **kwargs):
-        super(GroupUpdateForm, self).__init__(*args, **kwargs)
+        super(GroupForm, self).__init__(*args, **kwargs)
 
         self.helper = FormHelper(self)
 
+        # add form or edit form
+        if kwargs['instance'] is None:
+            add_form = True
+        else:
+            add_form = False
+
         # set form tag attributes
-        self.helper.form_action = reverse('groups_edit',
-            kwargs={'pk': kwargs['instance'].id})
+        if add_form:
+            self.helper.form_action = reverse('groups_add')
+        else:
+            self.helper.form_action = reverse('groups_edit',
+                kwargs={'pk': kwargs['instance'].id})
         self.helper.form_method = 'POST'
         self.helper.form_class = 'form-horizontal'
 
         # set form field properties
         self.helper.help_text_inline = True
-        self.helper.html5_required = True
         self.helper.label_class = 'col-sm-2 control-label'
         self.helper.field_class = 'col-sm-10'
 
         # add buttons
+        if add_form:
+            submit = Submit('add_button', u'Додати',
+                css_class="btn btn-primary")
+        else:
+            submit = Submit('save_button', u'Зберегти',
+                css_class="btn btn-primary")
         self.helper.layout[-1] = FormActions(
-            Submit('add_button', u'Зберегти', css_class="btn btn-primary"),
+            submit,
             Submit('cancel_button', u'Скасувати', css_class="btn btn-link"),
         )
 
@@ -81,27 +92,31 @@ class GroupUpdateForm(ModelForm):
                 code='invalid')
         return new_leader
 
-class GroupUpdateView(UpdateView):
-    model = Group
-    template_name = 'students/groups_edit.html'
-    form_class = GroupUpdateForm
+class BaseGroupFormView(object):
 
     def get_success_url(self):
-        return u'%s?status_message=Групу успішно збережено!' \
+        return u'%s?status_message=Зміни успішно збережено!' \
             % reverse('groups')
 
     def post(self, request, *args, **kwargs):
+        # handle cancel button
         if request.POST.get('cancel_button'):
-            return HttpResponseRedirect(
-                u'%s?status_message=Редагування групи відмінено!' %
-                reverse('groups'))
+            return HttpResponseRedirect(reverse('groups') +
+                u'?status_message=Зміни скасовано.')
         else:
-            return super(GroupUpdateView, self).post(request, *args, **kwargs)
+            return super(BaseGroupFormView, self).post(
+                request, *args, **kwargs)
 
-class GroupDeleteView(DeleteView):
+class GroupAddView(BaseGroupFormView, CreateView):
+    model = Group
+    form_class = GroupForm
+    template_name = 'students/groups_form.html'
+
+class GroupUpdateView(BaseGroupFormView, UpdateView):
+    model = Group
+    form_class = GroupForm
+    template_name = 'students/groups_form.html'
+
+class GroupDeleteView(BaseGroupFormView, DeleteView):
     model = Group
     template_name = 'students/groups_confirm_delete.html'
-
-    def get_success_url(self):
-        return u'%s?status_message=Групу успішно видалено!' \
-            % reverse('groups')
