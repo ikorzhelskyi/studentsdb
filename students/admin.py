e7e8946bb172fa6df25f967140b9a3da2c43580b
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-09-03 20:07:24 +0300

    Register model MonthJournal

--- students/admin.py
+++ students/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.core.urlresolvers import reverse
 from django.forms import ModelForm, ValidationError
 
-from .models import Student, Group
+from .models import Student, Group, MonthJournal
 
 class StudentFormAdmin(ModelForm):
 
@@ -70,3 +70,4 @@ class GroupAdmin(admin.ModelAdmin):
 
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Group, GroupAdmin)
+admin.site.register(MonthJournal)
