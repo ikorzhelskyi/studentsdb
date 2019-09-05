b37aa68d0bb69c3019dafe7280a5428bfb166ff1
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-09-05 16:40:09 +0300

    Update view for journal

--- students/views/journal.py
+++ students/views/journal.py
@@ -1,19 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
-def journal(request):
-    students = (
-        {'id': 1,
-         'first_name': u'Андрій',
-         'last_name': u'Вірт'},
-        {'id': 2,
-         'first_name': u'Юрій',
-         'last_name': u'Семчук'},
-        {'id': 3,
-         'first_name': u'Роман',
-         'last_name': u'Ярощак'},
-        )
-    return render(request, 'students/journal.html',
-        {'students': students})
+class JournalView(TemplateView):
+    template_name = 'students/journal.html'
