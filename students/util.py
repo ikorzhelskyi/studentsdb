2d8e94434490a0b8972d53dce12eb625b5c512b0
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-09-15 23:26:23 +0300

    Add function that returns selected group or None

--- students/util.py
+++ students/util.py
@@ -40,3 +40,20 @@ def get_groups(request):
             'selected': cur_group and cur_group.id == group.id and True or False
         })
     return groups
+
+def get_current_group(request):
+    """Returns currently selected group or None"""
+
+    # we remember selected group in a cookie
+    pk = request.COOKIES.get('current_group')
+
+    if pk:
+        from .models import Group
+        try:
+            group = Group.objects.get(pk=int(pk))
+        except Group.DoesNotExist:
+            return None
+        else:
+            return group
+    else:
+        return None
