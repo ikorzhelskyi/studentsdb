8c76117bc90dd2a885049913ff6db0bd9122e98a
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-09-19 15:49:40 +0300

    Add function for request to server and display editing form in modal window

--- students/static/js/main.js
+++ students/static/js/main.js
@@ -58,8 +58,49 @@ function initDateFields() {
   });
 }
 
+function initEditStudentPage() {
+  $('a.student-edit-form-link').click(function(event){
+    var link = $(this);
+    $.ajax({
+      'url': link.attr('href'),
+      'dataType': 'html',
+      'type': 'get',
+      'success': function(data, status, xhr){
+        // check if we got successfull response from the server
+        if (status != 'success') {
+          alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+          return false;
+        }
+
+        // update modal window with arrived content from the server
+        var modal = $('#myModal'),
+          html = $(data), form = html.find('#content-column form');
+        modal.find('.modal-title').html(html.find('#content-column h2').text());
+        modal.find('.modal-body').html(form);
+
+        // init our edit form
+        initEditStudentForm(form, modal);
+
+        // setup and show modal window finally
+        modal.modal({
+          'keyboard': false,
+          'backdrop': false,
+          'show': true
+        });
+      },
+      'error': function(){
+          alert('Помилка на сервері. Спробуйте будь-ласка пізніше.');
+          return false
+      }
+    });
+
+    return false;
+  });
+}
+
 $(document).ready(function(){
   initJournal();
   initGroupSelector();
   initDateFields();
+  initEditStudentPage();
 });
