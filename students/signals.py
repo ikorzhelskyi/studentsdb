acd8b6f5b03fa8d931d9d8654081ce1a1bcd1577
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-09-27 22:12:21 +0300

    Add logging for delete student

--- students/signals.py
+++ students/signals.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from .models import Student
@@ -19,3 +19,12 @@ def log_student_updated_added_event(sender, **kwargs):
     else:
         logger.info("Student updated: %s %s (ID: %d)", student.first_name,
             student.last_name, student.id)
+
+@receiver(post_delete, sender=Student)
+def log_student_deleted_event(sender, **kwargs):
+    """Writes information about deleted student into log file"""
+    logger = logging.getLogger(__name__)
+
+    student = kwargs['instance']
+    logger.info("Student deleted: %s %s (ID: %d)", student.first_name,
+        student.last_name, student.id)
