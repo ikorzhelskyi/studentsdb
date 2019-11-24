4217bd762dd8d2f982596c3b92fbdb2b483ebb9b
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-11-24 07:57:13 +0200

    Create users views

--- stud_auth/views.py
+++ stud_auth/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+
+class UsersListView(ListView):
+    model = User
+    template_name = 'registration/users_list.html'
+    context_object_name = 'users'
+
+
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'registration/profile.html'
+    context_object_name = 'user_info'
