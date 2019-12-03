48c58b740271d26c422fe5aa7f1bfb9d1aaf8e73
Ihor Korzhelskyi <ihork2109@gmail.com>
2019-12-03 20:01:03 +0200

    Add abillity for users to edit their own profile

--- stud_auth/views.py
+++ stud_auth/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.urlresolvers import reverse, reverse_lazy
+from django.utils.translation import ugettext as _
+from django.shortcuts import get_object_or_404
 
+from .models import StProfile
+from .forms import ProfileEditForm
 
 class UsersListView(ListView):
     model = User
@@ -13,3 +19,13 @@ class UserDetailView(DetailView):
     model = User
     template_name = 'registration/profile.html'
     context_object_name = 'user_info'
+
+class UserUpdateView(SuccessMessageMixin, UpdateView):
+    form_class = ProfileEditForm
+    template_name = 'registration/profile_edit.html'
+    success_message = _(u'Profile successfully updated!')
+    success_url = reverse_lazy('profile')
+
+    def get_object(self, queryset=None):
+        obj, created = StProfile.objects.get_or_create(user=self.request.user)
+        return obj
